2f53464298064c417357f0a4883cd95aa45e2334
sjproctor <sproctor950@gmail.com>
2020-08-28 13:40:00 -0700

    adds code challenges

---
+++ javascript-challenges.js
@@ -0,0 +1,94 @@
+// JavaScript Practice Challenges
+
+
+// Consider this variable:
+const headphones = [
+  {
+    brand: 'Skullcandy',
+    color: 'blue',
+    wireless: true,
+    price: 24.99
+  },
+  {
+    brand: 'Skullcandy',
+    color: 'black',
+    wireless: false,
+    price: 12.99
+  },
+  {
+    brand: 'Bose',
+    color: 'black',
+    wireless: true,
+    price: 149.99
+  },
+  {
+    brand: 'AKG',
+    color: 'black',
+    wireless: false,
+    price: 49.99
+  },
+  {
+    brand: 'Sennheiser',
+    color: 'black',
+    wireless: false,
+    price: 99.99
+  },
+  {
+    brand: 'Philips',
+    color: 'white',
+    wireless: false,
+    price: 10.99
+  }
+]
+
+
+// For each prompt, write a function that takes < headphones > as an argument and returns appropriate output.
+
+// Write a function that returns the cheapest set.
+// Write a function that returns the most expensive set.
+// Write a function called  < affordable > that returns all sets that cost less than 50.
+// Write a function called  < wirelessSets > that returns all wireless headphones.
+// Write a function called  < wiredSets > that returns all wired headphones.
+
+
+
+// Consider this variable:
+var sites = ["codepen.io", "codecademy.com", "coursera.org", "egghead.io", "udemy.com", "pluralsight.com", "udacity.com", "sitepoint.com"]
+
+// Write a function called  < dotCom > that takes in one string as an argument and determines if that string contains ".com" (Should return either true or false).
+
+// Write a function called  < loopOver > function that finds all ".com" websites in the sites array.
+
+// Call your loopOver function to find all ".io" websites in the sites array.
+
+// Call your loopOver function to find all ".org" websites in the sites array.
+
+
+
+// Iteration: while/do while loops
+
+// Write a while loop that logs each number from 0 to 10.
+// Write a while loop that logs each number from 10 to 0.
+// Write a do while loop that logs each number from 0 to 10
+// Write a do while loop that logs each number from 10 to 0.
+
+// Write a function that takes a number as an argument and uses a while loop to count up to that number from 0.
+// Write a function that takes a number as an argument and uses a do while loop to count up to that number from 0.
+
+
+// Iteration: map/filter
+
+// Consider this variable:
+var people = [
+  { firstName: "Ada", lastName: "Lovelace" },
+  { firstName: "Tim", lastName: "Berners-Lee" },
+  { firstName: "Alan", lastName: "Turing" }
+  { firstName: "Grace", lastName: "Hopper" }
+  { firstName: "Linus", lastName: "Torvalds" }
+  { firstName: "Bill", lastName: "Gates" }
+  { firstName: "Yukihiro", lastName: "Matsumoto" }
+  { firstName: "David", lastName: "Heinemeier Hansson" }
+]
+// Write a function that returns just the first name of each person.
+// Write a function that returns only the names that start with vowels.
+// Write a function that returns an array of objects so that each object has a key for firstName, lastName, and fullName with the appropriate values.
